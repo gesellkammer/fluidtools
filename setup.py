7ee832ee88fe05c86f14539f358e44dbcc734b14
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2021-06-13 17:04:45 +0200

    setup

--- setup.py
+++ setup.py
@@ -4,6 +4,34 @@ from setuptools import setup, Extension
 
 VERSION = '0.1.0'
 
+include_dirs = []
+library_dirs = []
+compile_args = []
+
+if platform == 'Darwin':
+    include_dirs.append("/usr/local/include/")
+    include_dirs.append("/opt/local/include/")
+
+    library_dirs.append("/usr/local/lib")
+    library_dirs.append("/opt/local/lib")
+
+    compile_args += [
+        '-fno-strict-aliasing',
+        '-Werror-implicit-function-declaration',
+        '-Wfatal-errors'
+    ]
+elif platform == 'Linux':
+    include_dirs.extend(['usr/include', '/usr/local/include'])
+    library_dirs.append("/usr/local/lib")
+    compile_args += [
+        '-fno-strict-aliasing',
+        '-Werror-implicit-function-declaration',
+        '-Wfatal-errors'
+    ]
+else:
+    pass
+
+
 setup(
     name='fluidtools',
     python_requires='>=3.8',
@@ -13,6 +41,9 @@ setup(
             'fluidtools', 
             sources = ['src/fluidtools.pyx'],
             libraries=['fluidsynth'],
+            library_dirs=library_dirs,
+            include_dirs=include_dirs,
+            extra_compile_args=compile_args
         )
     ],
     setup_requires=['setuptools>=18', 'cython'],
